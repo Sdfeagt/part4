f955f20d1eaa54303754bf978599677d85bf9edb
Sdfeagt <rafalciechanski15@gmail.com>
2022-08-07 19:22:06 +0300

    4.5 done

--- 4.1-4.23/tests/blog.test.js
+++ 4.1-4.23/tests/blog.test.js
@@ -24,3 +24,36 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+describe('Most liked', () =>{
+  const listWithtwoBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: 'somethingidk',
+      title: 'Oh look, a bird!',
+      author: 'My dog, Felo',
+      url: 'Wedonthavethemoneyforthat.com',
+      likes: 50,
+      __v: 0
+    }
+  ]
+
+  test('Should be obj with 50 likes', () =>{
+    const result = listHelper.favoriteBlog(listWithtwoBlog)
+    expect(result).toStrictEqual(    {
+      _id: 'somethingidk',
+      title: 'Oh look, a bird!',
+      author: 'My dog, Felo',
+      url: 'Wedonthavethemoneyforthat.com',
+      likes: 50,
+      __v: 0
+    })
+  })
+})
--- 4.1-4.23/utils/list_helper.js
+++ 4.1-4.23/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog");
+
 const dummy = (blogs) => {
   return 1
 }
@@ -10,10 +12,25 @@ const sumWithInitial = likesarray.reduce(
   (previousValue, currentValue) => previousValue + currentValue, initialValue)
 
   return sumWithInitial
+}
+
+const favoriteBlog = (blogs) =>{
+  const likesarray = blogs.map(bloglikes => bloglikes.likes)
+  const max = Math.max(...likesarray)
+  console.log("From test function: ", likesarray);
+  console.log("From test function: ", max);
+
+  const found = blogs.find(blog =>{
+    return blog.likes === max
+  });
+  console.log("From test function: ", found);
+
+  return found
 
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
