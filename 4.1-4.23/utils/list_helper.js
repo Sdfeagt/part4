a2e2544316b35fc60fa202c15e67c6703eb57f8b
Sdfeagt <rafalciechanski15@gmail.com>
2022-08-07 16:33:18 +0300

    4.4 done

--- 4.1-4.23/utils/list_helper.js
+++ 4.1-4.23/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) =>{
+  const likesarray = blogs.map(bloglikes => bloglikes.likes)
+
+const initialValue = 0;
+const sumWithInitial = likesarray.reduce(
+  (previousValue, currentValue) => previousValue + currentValue, initialValue)
+
+  return sumWithInitial
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
