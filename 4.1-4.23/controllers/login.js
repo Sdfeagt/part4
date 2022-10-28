aaf6bf5c4fb3f139621cbfe7f1ce32d778a4e10e
Sdfeagt <rafalciechanski15@gmail.com>
2022-10-28 14:38:34 +0300

    -

--- 4.1-4.23/controllers/login.js
+++ 4.1-4.23/controllers/login.js
@@ -7,8 +7,6 @@ loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
 
   const user = await User.findOne({ username })
-  console.log(user.passwordHash);
-  console.log(password);
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
